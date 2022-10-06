7c17050c600b6b9c5576eeea03da5e05baac93cd
=Lucas blake <=lucbl1105@gmail.com>
2022-10-06 12:41:33 -0300

    inital commit

---
+++ js/header.js
@@ -0,0 +1,8 @@
+var loged = 0
+const fetchheader = (elementtofetch) => { fetch(elementtofetch).then(response => response.text()).then(data => document.getElementById("header_main").innerHTML += data); };
+headerused = loged == "pato" ? fetchheader("navbars/logged_header.html") : fetchheader("navbars/unlogged_header.html");
+var headerimg = document.getElementById("header_img");
+var data = document.getElementById("header_PersonalInfo");
+function showd() {
+  data.lastElementChild.style.display == "none" ? data.lastElementChild.style.display = "flex" : data.lastElementChild.style.display = "none";
+}
