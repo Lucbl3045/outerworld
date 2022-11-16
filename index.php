f17fe7bbc2378de32a32ddc387d1a5eb3d599258
Your Name <you@example.com>
2022-11-16 12:29:59 -0300

    php basico y feed

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/header_style.css">
+  <link rel="stylesheet" href="css/footer_style.css">
+  <title>OuterWorld</title>
+
+</head>
+<!--
+<style> 
+
+   background-color: rgb(189, 3, 3);
+  
+</style>
+-->
+
+<body>
+  <header id="header_main">
+  <?php isset($_SESSION['user'])? require_once("./navbars/logged_header.html") : require_once("./navbars/unlogged_header.html"); ?>
+  </header>
+  <div id="container">
+  <?php 
+  if(isset($_SESSION['user'])){
+    require("./views/feed.php");
+  }
+    else {
+      require("./views/whoweare.html");
+  }
+  ?>
+  <footer id="footer_main">
+    <h4 style="color:white ;">Copyright <span><a href="www.lego.com">@LucasB2</a></span></h4>
+    <h4>pato</h4>
+  <div class="relations"><a href="twitter.com/" ></a><a href="twitter.com/" ></a><a href="twitter.com/" ></a></div>
+  </footer>
+</body>
+<script src="./js/header.js"></script>
+
+</html>
--- navbars/logged_header.php
+++ navbars/logged_header.php
@@ -1,3 +1,4 @@
+
 <ul id="header_PersonalInfo">
   <li><button onclick="showd()" id="personal_info_button">
       <img src="./image/unlogged_img.jpg" alt="profile_picture" id="header_img">
@@ -6,9 +7,12 @@
   </li>
   <li>
     <div id="personal_info">
+      <div class="img">
+        <img src="<?php echo "./image/".$_SESSION['userimage']."jpg"?>" alt="userPhoto">
+      </div>
       <div style="align-self:center ;">
         <h5>User_Name</h5>
-
+        <h5><?php echo ($_SESSION['user']); ?></h5>
       </div>
 
       <button>account info</button>
